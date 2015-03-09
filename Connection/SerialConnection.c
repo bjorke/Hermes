c4122600c1d521319b2d75d570c9e0e713bf30a9
Ludwig Björk <gusbjorklu@student.gu.se>
2015-03-09 22:09:48 +0100

    added a working connection
    the output looks like…
    DOUBLE
    123.36
    2.10
    4.00
    5.00
    5.50
    72.00
    21.00
    52.41
    12.00
    25.00
    INT
    524
    6987
    54
    59
    456
    5468
    52
    214
    978
    0

--- Connection/SerialConnection.c
+++ Connection/SerialConnection.c
@@ -1,162 +1,88 @@
-#include <stdio.h>    /* Standard input/output definitions */
-#include <stdlib.h>
-#include <stdint.h>   /* Standard types */
-#include <string.h>   /* String function definitions */
-#include <unistd.h>   /* UNIX standard function definitions */
-#include <fcntl.h>    /* File control definitions */
-#include <errno.h>    /* Error number definitions */
-#include <termios.h>  /* POSIX terminal control definitions */
-#include <sys/ioctl.h>
-#include <getopt.h>
-#include <sys/shm.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <unistd.h>
+#define BAUDRATE B9600
+#define MODEMDEVICE "/dev/tty.usbmodem1411"/*UART NAME IN PROCESSOR*/
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+void openport(void);
+void sendport(void);
+void readport(void);
+int fd=0, n;
+static int cnt, size, s_cnt;
+unsigned char *var;
+struct termios oldtp, newtp;
+char sendcmd1[10]="\0";
+FILE *file;
 
-void usage(void);
-int serialport_init(char* serialport, int baud);
-int serialport_read_until(int fd, char* buf, char until);
-int shareMem();
+void  readport(void){
+	unsigned char buff;
+	file = fopen( "output.txt", "w+" );
+  int looper = 0;
+  while (looper < 1000) {
+    n = read(fd, &buff, 1);
+    //	fcntl(fd,F_SETFL,0);
+    if (n == -1) switch(errno) {
+    case EAGAIN: /* sleep() */
+      continue;
 
-char* serialport = "/dev/tty.usbmodem1411";
-
-int main(int argc, char *argv[])
-{
-    int fd = 0;
-    int baudrate = B9600;  // default
-    char buf[256];
-    int rc,n;
-    int option_index = 0, opt;
-
-    fd = serialport_init(serialport,9600);
-
-    serialport_read_until(fd, buf, '\n');
-    printf("read: %s\n",buf);
-
-    shareMem();
-
-    exit(EXIT_SUCCESS);
-} // end main
-int shareMem(){
-    int runner = 1;
-    key_t shm_key = ftok("./somefile", 'b');
-    const int shm_size = 1024;
-
-    int shm_id;
-    char* shmaddr, *ptr;
-    int next[2];
-
-    printf ("writer started.\n");
-      while(runner == 1){
-
-      /* Allocate a shared memory segment. */
-      shm_id = shmget (shm_key, shm_size, IPC_CREAT | S_IRUSR | S_IWUSR);
-
-      /* Attach the shared memory segment. */
-      shmaddr = (char*) shmat (shm_id, 0, 0);
-
-      printf ("shared memory attached at address %p\n", shmaddr);
-
-      /* Start to write data. */
-      ptr = shmaddr + sizeof (next);
-      next[0] = sprintf (ptr, "mandy") + 1;
-      ptr += next[0];
-      next[1] = sprintf (ptr, "73453916") + 1;
-      ptr += next[1];
-      sprintf (ptr, "amarica");
-      memcpy(shmaddr, &next, sizeof (next));
-      printf ("writer ended.\n");
-
-      /*calling the other process*/
-      //system("./read.out");
-      sleep(10);
-    };
-    /* Detach the shared memory segment. */
-    shmdt (shmaddr);
-    /* Deallocate the shared memory segment.*/
-    shmctl (shm_id, IPC_RMID, 0);
-
-    return 0;
+    }
+    if (n ==0) break;
+    fputc(buff, file);
+    printf("%d \"%c\"\n", n,buff);
+    looper++;
+  }
+  fclose (file);
 }
-int serialport_read_until(int fd, char* buf, char until)
-{
-    char b[1];
-    int i=0;
-    do {
-        int n = read(fd, b, 1);  // read a char at a time
-        if( n==-1) return -1;    // couldn't read
-        if( n==0 ) {
-            usleep( 10 * 1000 ); // wait 10 msec try again
-            continue;
-        }
-        buf[i] = b[0]; i++;
-    } while( b[0] != until );
+void openport(void){
 
-    buf[i] = 0;  // null terminate the string
-    return 0;
+	 fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY |O_NDELAY );
+	 printf("oviya :%d\n",fd);
+   if (fd <0){
+   	perror(MODEMDEVICE);
+
+   }
+
+   fcntl(fd,F_SETFL,0);
+   tcgetattr(fd,&oldtp); /* save current serial port settings */
+   bzero(&newtp, sizeof(newtp));
+
+   newtp.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
+
+   newtp.c_iflag = IGNPAR | ICRNL;
+
+   newtp.c_oflag = 0;
+   newtp.c_lflag = ICANON;
+   newtp.c_cc[VINTR]    = 0;     /* Ctrl-c */
+   newtp.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
+   newtp.c_cc[VERASE]   = 0;     /* del */
+   newtp.c_cc[VKILL]    = 0;     /* @ */
+  // newtp.c_cc[VEOF]     = 4;     /* Ctrl-d */
+   newtp.c_cc[VTIME]    = 0;     /* inter-character timer unused */
+   newtp.c_cc[VMIN]     = 0;     /* blocking read until 1 character arrives */
+   //newtp.c_cc[VSWTC]    = 0;     /* '\0' */
+   newtp.c_cc[VSTART]   = 0;     /* Ctrl-q */
+   newtp.c_cc[VSTOP]    = 0;     /* Ctrl-s */
+   newtp.c_cc[VSUSP]    = 0;     /* Ctrl-z */
+   newtp.c_cc[VEOL]     = 0;     /* '\0' */
+   newtp.c_cc[VREPRINT] = 0;     /* Ctrl-r */
+   newtp.c_cc[VDISCARD] = 0;     /* Ctrl-u */
+   newtp.c_cc[VWERASE]  = 0;     /* Ctrl-w */
+   newtp.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
+   newtp.c_cc[VEOL2]    = 0;     /* '\0' */
+
+
 }
 
-int serialport_init(char* serialport, int baud)
-{
+int main(int argc, char *argv[])    {
 
-    printf("serialport.. %s\n",serialport);
-    printf("baud.. %i\n",baud);
-
-    struct termios toptions;
-    int fd;
-
-    //fprintf(stderr,"init_serialport: opening port %s @ %d bps\n",
-    //        serialport,baud);
-
-    fd = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
-    if (fd == -1)  {
-        perror("init_serialport: Unable to open port ");
-        return -1;
-    }
-
-    if (tcgetattr(fd, &toptions) < 0) {
-        perror("init_serialport: Couldn't get term attributes");
-        return -1;
-    }
-    speed_t brate = baud; // let you override switch below if needed
-    switch(baud) {
-    case 4800:   brate=B4800;   break;
-    case 9600:   brate=B9600;   break;
-#ifdef B14400
-    case 14400:  brate=B14400;  break;
-#endif
-    case 19200:  brate=B19200;  break;
-#ifdef B28800
-    case 28800:  brate=B28800;  break;
-#endif
-    case 38400:  brate=B38400;  break;
-    case 57600:  brate=B57600;  break;
-    case 115200: brate=B115200; break;
-    }
-    cfsetispeed(&toptions, brate);
-    cfsetospeed(&toptions, brate);
-
-    // 8N1
-    toptions.c_cflag &= ~PARENB;
-    toptions.c_cflag &= ~CSTOPB;
-    toptions.c_cflag &= ~CSIZE;
-    toptions.c_cflag |= CS8;
-    // no flow control
-    toptions.c_cflag &= ~CRTSCTS;
-
-    toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
-    toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
-
-    toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
-    toptions.c_oflag &= ~OPOST; // make raw
-
-    // see: http://unixwiz.net/techtips/termios-vmin-vtime.html
-    toptions.c_cc[VMIN]  = 0;
-    toptions.c_cc[VTIME] = 20;
-
-    if( tcsetattr(fd, TCSANOW, &toptions) < 0) {
-        perror("init_serialport: Couldn't set term attributes");
-        return -1;
-    }
-
-    return fd;
+	openport();
+	readport();
+        return 0;
 }
--- Tester/Tester.ino
+++ Tester/Tester.ino
@@ -1,11 +1,20 @@
-int test = 1;
-int bytesSent = 0;
+char *startDouble = "DOUBLE";
+double test[10] = {123.35546,2.1,4,5,5.5,72,21,52.41,12,25};
+char *startInt = "INT";
+int test1[10] = {524,6987,54,59,456,5468,52,214,978};
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  bytesSent = Serial.write(test);
-  Serial.println(bytesSent);
-
-  delay(500);
+  Serial.println(startDouble);
+  for (int i = 0; i < 10; i = i + 1) {
+  Serial.println(test[i]);
+  };
+  
+  Serial.println(startInt);
+  for (int i = 0; i < 10; i = i + 1) {
+  Serial.println(test1[i]);
+  };
+  
+  delay(10);
 }
