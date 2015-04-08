99f7c3e8444f5d62a54bc4bb3ad4e45ce72cc83c
Ludwig Björk <gusbjorklu@student.gu.se>
2015-04-08 14:21:46 +0200

    added IR sensors

--- Arduino/arduinoSend.ino
+++ Arduino/arduinoSend.ino
@@ -8,6 +8,8 @@ Checksum _Checksum;
 #define sonar2EchoPin 12
 #define sonar3TrigPin 13
 #define sonar3EchoPin 12
+#define ir1Port 0
+#define ir2Port 1
 
 void setup() {
   Serial.begin(9600);
@@ -23,21 +25,43 @@ void setup() {
 }
 
 void loop() {
+  //sonar setup
   short int sonar1,sonar2,sonar3;
   sonar1 = sonar(sonar1EchoPin,sonar1TrigPin);
   sonar2 = sonar(sonar2EchoPin,sonar2TrigPin);
   sonar3 = sonar(sonar3EchoPin,sonar3TrigPin);
+  //ir setup
+  short int ir1, ir2;
+  ir1 = ir(ir1Port);
+  ir2 = ir(ir2Port);
+  //Generate and send message
+  Serial.println(generateMessage(sonar1,sonar2,sonar3,ir1,ir2));
 
-  //#####################
-  //Only for testing
-  sonar2 = sonar2 *1.564;
-  sonar3 = sonar3 *0.654;
-  //#####################
-
+  delay(1);
+}
+short int ir(short int irPort){
+  short int ir;
+  ir = analogRead (irPort);
+  return ((short int)ir / 615) * 1024;
+}
+short int sonar(short int echo,short int trigger){
+  short int duration;
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  duration = pulseIn(echo, HIGH);
+  return (duration/2) / 29.1;
+}
+String generateMessage(short int sonar1,short int sonar2,short int sonar3,short int ir1,short int ir2){
+  //checksum
   char sumCheck;
   sumCheck = sonar1;
   sumCheck += sonar2;
   sumCheck += sonar3;
+  sumCheck += ir1;
+  sumCheck += ir2;
   short int checkSum = _Checksum.generate_verhoeff(&sumCheck);
 
   String sendString = "!";
@@ -47,22 +71,10 @@ void loop() {
   sendString += ",";
   sendString += sonar3;
   sendString += ",";
+  sendString += ir1;
+  sendString += ",";
+  sendString += ir2;
+  sendString += ",";
   sendString += checkSum;
-  Serial.println(sendString);
-
-  delay(1);
-}
-
-short int sonar(short int echo,short int trigger){
-  short int duration,distance;
-
-  digitalWrite(trigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger, LOW);
-  duration = pulseIn(echo, HIGH);
-  distance = (duration/2) / 29.1;
-
-  return distance;
+  return sendString;
 }
